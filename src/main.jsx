c5ab24cc3bf97d5488fe549250854c933bb97f26
SO IN <jg.hardcore.builds@gmail.com>
2025-09-14 20:25:24 -0300

    new video v2

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './fonts.css' // <-- AÑADE ESTA LÍNEA
 import './index.css'
+import { Analytics } from '@vercel/analytics/react'; // <-- AÑADE ESTA LÍNEA
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Analytics /> {/* <-- AÑADE ESTA LÍNEA */}
   </React.StrictMode>,
 )
